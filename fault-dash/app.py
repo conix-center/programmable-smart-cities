db9b74fedaa1786e7acec7050ed6967f722086c4
Reapor-Yurnero <reapor.yurnero@gmail.com>
2020-09-27 16:18:06 -0700

    add vav fault detection draft

--- fault-dash/app.py
+++ fault-dash/app.py
@@ -31,6 +31,8 @@ def update(interval):
     impls.append(VAVDemoFault(5))
     from faults.rogue_zone_temp import RogueZoneTemp
     impls.append(RogueZoneTemp("ciee", "ciee"))
+    from faults.vav_airflow import VAVAirflow
+    impls.append(VAVAirflow())
 
     historical_ranges = pd.date_range('2018-01-03', '2019-01-01', freq='24H')
     historical_idx = 0
@@ -47,6 +49,7 @@ def update(interval):
         statusLock.release()
         time.sleep(interval)
         historical_idx += 1
+        break
 
 
 @app.route('/', methods=['GET'])
